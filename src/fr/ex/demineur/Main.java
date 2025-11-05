0dae2a76e20b7c109a7223a9544d508172337dd9
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-11-05 10:18:45 +0100

    ajout de la grille (6x12)

--- src/fr/ex/demineur/Main.java
+++ src/fr/ex/demineur/Main.java
@@ -2,8 +2,24 @@ package fr.ex.demineur;
 
 public class Main {
 
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		char[][] grille = new char[6][12];
+
+		for (int i = 0; i < grille.length; i++) {
+		    for (int j = 0; j < grille[i].length; j++) {
+		        grille[i][j] = '_';
+		    }
+		}
+		
+		// Affichage de la grille
+        for (int i = 0; i < grille.length; i++) {
+            for (int j = 0; j < grille[i].length; j++) {
+                System.out.print(grille[i][j] + "\t");
+            }
+            System.out.println();
+        }
 
 	}
 
